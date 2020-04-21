b01bb3ce5f08504c8cfabc4c46bac0e5dae56255
Apple <apple@Apples-MacBook-Air.local>
2020-04-21 14:19:33 +0530

    first commit

---
+++ src/components/MainComponent.js
@@ -0,0 +1,5 @@
+import React, { Component } from 'react';
+import { Navbar, NavbarBrand } from 'reactstrap';
+import Menu from './MenuComponent';
+import DishDetail from './DishdetailComponent';
+import { DISHES } from '../shared/dishes';
